abf6ac83bd9130dba3c3bb3b5f4f278ac1ac827e
Emanuel <emanuelbrit16@gmail.com>
2025-11-26 14:43:53 -0300

    Creating Javalin Config

--- projeto-pizzaria/src/main/java/br/gov/sp/fatec/pg/Main.java
+++ projeto-pizzaria/src/main/java/br/gov/sp/fatec/pg/Main.java
@@ -1,7 +1,17 @@
 package br.gov.sp.fatec.pg;
 
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Criando app
+        Javalin app = Javalin.create().start(7070);
+        System.out.println("Javalin rodando com sucesso!");
+
+        // Rota base
+        app.get("/", ctx -> {
+            ctx.result("Rodando na porta 7070");
+        });
     }
 }
